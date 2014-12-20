d9f69182df9a1b85408dbb1d69cb77072d391d6d
wxrighttoleft <zhuiqiuxinf@163.com>
2014-12-20 21:29:28 +0800

    wx

--- src/com/wx/main/Main.java
+++ src/com/wx/main/Main.java
@@ -1,10 +1,26 @@
 package com.wx.main;
 
+import java.util.Scanner;
+
 public class Main {
-
+	private final static int cz = 8 * 60;
+	private final static int dz = 24 * 60;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		int rs = scan.nextInt();
+		scan.close();
+		int h = rs / 100;
+		int m = rs - h * 100;
+		if(h < 8){
+			int sumM = h * 60 + m;
+			int c = sumM - cz;
+			int sjm = dz + c;
+			h = sjm / 60;
+			m = sjm % 60;
+		}else{
+			h -= 8;
+		}
+		System.out.println(h * 100 + m);
 	}
 
 }
